540c3356010376a188d9b8fbe474fe4ac91bcc29
Easy <easychen@qq.com>
2025-02-23 19:04:02 +0800

    3.0.3版本，修复客户端、修正bug

--- FlowDeerTree/src-tauri/src/lib.rs
+++ FlowDeerTree/src-tauri/src/lib.rs
@@ -7,6 +7,8 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_upload::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
